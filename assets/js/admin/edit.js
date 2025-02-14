c9a0407ce69b2cedb33eb12789848852d93d04df
Aymane <its.aymane.bouljam@gmail.com>
2025-02-14 16:49:42 +0100

    Handling piece edit

--- assets/js/admin/edit.js
+++ assets/js/admin/edit.js
@@ -1,13 +1,11 @@
-const piècesList = JSON.parse(localStorage.getItem("piècesList"));
-
-
 // fill the form
-const urlParams = new URLSearchParams(window.location.search);
-
-const uncoded_param = urlParams.get("designation");
-const param = decodeURIComponent(uncoded_param);
-
 const fillForm = () => {
+    const piècesList = JSON.parse(localStorage.getItem("piècesList"));
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const uncoded_param = urlParams.get("designation");
+    const param = decodeURIComponent(uncoded_param);
+
     const designation = document.getElementById("désignation");
     const prix_unitaire = document.getElementById("prix_unitaire");
 
@@ -22,3 +20,27 @@ const fillForm = () => {
 }
 
 window.onload = ()=>fillForm();
+
+const editPiece= (event) => {
+    event.preventDefault();
+    const designation = document.getElementById("désignation");
+    const prix_unitaire = document.getElementById("prix_unitaire");
+
+    const piècesList = JSON.parse(localStorage.getItem("piècesList"));
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const uncoded_param = urlParams.get("designation");
+    const param = decodeURIComponent(uncoded_param);
+
+    if(piècesList){
+        const newData = piècesList.map(pièce => {
+            if(pièce.designation.toLowerCase() === param.toLowerCase()){
+                return {...pièce, designation : designation.value, prix_unitaire : prix_unitaire.value};
+            }else{
+                return pièce;
+            }
+        });
+        localStorage.setItem("piècesList", JSON.stringify(newData));
+        window.location = "admin.html";
+    }
+}
