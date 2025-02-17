a54717c36388a91ee513b297f6de94da120985a6
Aymane <its.aymane.bouljam@gmail.com>
2025-02-17 23:31:11 +0100

    updating seed

--- assets/db/seed.js
+++ assets/db/seed.js
@@ -3,7 +3,7 @@ const seedPieces = () => {
     if(confirm("Cette action va supprimer les données actuelles, voulez-vous continuer ?")){
         const pieces = [
             { "designation": "Collier AC PVC 60*20", "prix_unitaire": 75.01 },
-            { "designation": "Collier AC PVC 63*20", "prix_unitaire": 65.03 },
+            { "designation": "Collier AC PVC 63*20", "prix_unitaire": 74.04 },
             { "designation": "Collier AC PVC 80*40", "prix_unitaire": 72.81 },
             { "designation": "Collier AC PVC 60*40", "prix_unitaire": 70.04 },
             { "designation": "Collier AC PVC 100*40", "prix_unitaire": 79.39 },
@@ -14,9 +14,9 @@ const seedPieces = () => {
             { "designation": "Tuyau Poly HD 10k25", "prix_unitaire": 3.81 },
             { "designation": "Tuyau Poly HD 10k20", "prix_unitaire": 7.5 },
             { "designation": "Couvercle tabernacle", "prix_unitaire": 7.5 },
-            { "designation": "Tube PVC 6K DN 75", "prix_unitaire": 52.09 },
-            { "designation": "Bouche à clée", "prix_unitaire": 19.90 },
-            { "designation": "Porte de niche GM", "prix_unitaire": 136.36 },
+            { "designation": "Tube PVC 6K DN 75", "prix_unitaire": 11.75 },
+            { "designation": "Bouche à clé", "prix_unitaire": 52.09},
+            { "designation": "Porte de niche GM", "prix_unitaire": 146.36 },
             { "designation": "Raccord SRM L 25*1/2", "prix_unitaire": 21.58 },
             { "designation": "Raccord SRM L 25*3/4", "prix_unitaire": 19.56 },
             { "designation": "Raccord SRM L 50*1*1/2", "prix_unitaire": 123.09 },
@@ -41,10 +41,6 @@ const seedPieces = () => {
 const seedFees = () => {
    if(confirm("Cette action va supprimer les données actuelles, Voulez-vous continuer ?")){
         const elements = [
-            {designation:"Pose Tuyau pol HD 20", valeur : 26.70},
-            {designation:"Pose Tuyau pol HD 25", valeur : 26.70},
-            {designation:"Pose Tuyau pol HD 50", valeur : 26.70},
-            {designation:"Pose Appareils", valeur : 133.56},
             {designation:"Installation de la prise", valeur : 191.37},
             {designation: "Tva", valeur : 0.20},
             {designation:"Frais intervention", valeur : 0.15},
