edcbb9e6d3624bb81a8f3babe939fd92a967bad5
Aymane <its.aymane.bouljam@gmail.com>
2025-02-19 22:16:49 +0100

    adding height & width & number of floors input fields

--- assets/js/formulaire.js
+++ assets/js/formulaire.js
@@ -56,12 +56,19 @@ const handleSelect = (event) => {
 const handleRiveraine = (event) => {
     const reviraine = Number(event.target.value);
     const motifContainer = document.getElementById("motifContainer");
+    const surfaceContainer = document.getElementById("surfaceContainer");
     if(reviraine !== 0){
         motifContainer.innerHTML = "";
+        surfaceContainer.innerHTML = `
+            <input type="text" class="form-control" placeholder="Longueur" id="longueur" required>
+            <input type="text" class="form-control"  placeholder="Largeur" id="largeur"  required>
+            <input type="text" class="form-control w-100"  placeholder="Nombre des étages" id="étages"  required>
+        `;
     }else if(reviraine == 0){
         motifContainer.innerHTML = `
             <input type="text" class="form-control" id="motif" placeholder="Motif de dispense">
         `;
+        surfaceContainer.innerHTML = "";
     }
 }
 
